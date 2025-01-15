641eec24e08ab5c36ef3f5e8cf84e1f08d331d8f
dorilama <assioma.xi@gmail.com>
2025-01-15 10:41:48 +0000

    feat: add schema for accounts and links noteOwner and accountOwner

--- packages/instant/instant.schema.ts
+++ packages/instant/instant.schema.ts
@@ -15,12 +15,24 @@ const _schema = i.schema({
       label: i.string().optional(),
       createdAt: i.number().optional().indexed(),
     }),
+    accounts: i.entity({
+      createdAt: i.number().optional().indexed(),
+    }),
   },
   // You can define links here.
   // For example, if `posts` should have many `comments`.
   // More in the docs:
   // https://www.instantdb.com/docs/schema#defining-links
-  links: {},
+  links: {
+    noteOwner: {
+      forward: { on: "notes", has: "one", label: "owner" },
+      reverse: { on: "$users", has: "many", label: "notes" },
+    },
+    accountOwner: {
+      forward: { on: "accounts", has: "one", label: "owner" },
+      reverse: { on: "$users", has: "one", label: "account" },
+    },
+  },
   // If you use presence, you can define a room schema here
   // https://www.instantdb.com/docs/schema#defining-rooms
   rooms: {},
